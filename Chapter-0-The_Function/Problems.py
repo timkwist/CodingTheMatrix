5eaffbdd76f8d90312d79685749ef42d13027717
Tim Kwist <Timkwist@hotmail.com>
2015-12-08 19:45:30 -0800

    Problem 0.8.2

--- Chapter-0-The_Function/Problems.py
+++ Chapter-0-The_Function/Problems.py
@@ -9,3 +9,13 @@ def increments(L):
 	return [num + 1 for num in L]
 # >>> Problems.increments([1,5,7])
 # [2, 6, 8]
+
+# Problem 0.8.2
+# cubes(L)
+# input: list L of numbers
+# output: list of numbers in which the ith element is the cube of the ith element of L
+# Example: given [1,2,3] return [1,8,27]
+def cubes(L):
+	return [num*num*num for num in L]
+# >>> Problems.cubes([1,2,3])
+# [1, 8, 27]
