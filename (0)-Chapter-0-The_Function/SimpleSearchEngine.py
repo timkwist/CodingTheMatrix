80af8eb9f82cdbf44b0fac31e8ce25e0d85158c9
Tim Kwist <Timkwist@hotmail.com>
2015-12-08 19:04:42 -0800

    Add orSearch; edit print statements and utilize python3 functions
    DocumentNumber is now appended
    Now using python3 print
    Now printing variables along with output for more context

--- (0)-Chapter-0-The_Function/SimpleSearchEngine.py
+++ (0)-Chapter-0-The_Function/SimpleSearchEngine.py
@@ -8,11 +8,31 @@ def makeInverseIndex(strlist):
 		words = document.split()
 		for word in words:
 			if word in dictionary:
-				dictionary[word] = dictionary[word] + [documentNumber]
+				dictionary[word].append(documentNumber)
 			else:
 				dictionary[word] = list({documentNumber})
 	return dictionary
 
-documents = ["one", "two", "one two"]
-print makeInverseIndex(documents)
+# Takes in an inverse index and list of words, and returns the set of document numbers specifying all documents
+# that contain any of the words in query
+def orSearch(inverseIndex, query):
+	queryWordsInIndex = set()
+	words = query.split()
+	for word in words:
+		if word in inverseIndex:
+			queryWordsInIndex.update(inverseIndex[word])
+	return queryWordsInIndex
+
+
+documents = ["Here is quite a few words", "and a few more", "and some more"]
+print("Initial document: ", documents)
+# Test makeInverseIndex
+index = makeInverseIndex(documents)
+print("Test makeInverseIndex: ", index)
 # Output: {'two': [1, 2], 'one': [0, 2]}
+
+# Test orSearch
+query = "more"
+print("Initial query: ", query)
+searchResults = orSearch(index, query)
+print("Test orSearch: ", searchResults)
