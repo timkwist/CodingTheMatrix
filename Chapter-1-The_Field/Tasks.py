309bbb5e79ab117fe44b8e97ae2f6bf0e03dee84
Tim Kwist <timkwist@hotmail.com>
2015-12-29 19:45:22 -0800

    Add Task 1.4.3

--- Chapter-1-The_Field/Tasks.py
+++ Chapter-1-The_Field/Tasks.py
@@ -1,5 +1,8 @@
 # Author: Tim Kwist
 
+from plotting import plot
+S = set({2 + 2j, 3 + 2j, 1.75 + 1j, 2 + 1j, 2.25 + 1j, 2.5 + 1j, 2.75 + 1j, 3 + 1j, 3.25 + 1j})
+
 # Task 1.4.1
 # Assign to the variable S a list or set consisting of the complex numbers:
 # S = {2 + 2i, 3 + 2i, 1.75 + 1i, 2 + 1i, 2.25 + 1i, 2.5 + 1i, 2.75 + 1i, 3 + 1i, 3.25 + 1i}
@@ -14,6 +17,11 @@
 # imaginary parts have the absolute value less than 4. The scale argument is optional and
 # defaults to 1, and there is another optional argument that sets the size of the dots.
 def task141():
-	from plotting import plot
-	S = set({2 + 2j, 3 + 2j, 1.75 + 1j, 2 + 1j, 2.25 + 1j, 2.5 + 1j, 2.75 + 1j, 3 + 1j, 3.25 + 1j})
 	plot(S, 4)
+
+# Task 1.4.3
+# Create a new plot using a comprehension to provide a set of points derived from S by adding
+# 1 + 2j to each:
+# >>> plot({1+2j+z for z in S}, 4)
+def task143():
+	plot({1+2j+z for z in S}, 4)
