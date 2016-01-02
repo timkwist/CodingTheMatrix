aba024de4b68ccb257967e987a7ea1e2814ae202
Tim Kwist <timkwist@hotmail.com>
2016-01-01 23:06:10 -0800

    Add Problem 1.7.2

--- Chapter-1-The_Field/Problems.py
+++ Chapter-1-The_Field/Problems.py
@@ -9,3 +9,11 @@
 # Example: given list = [1,2,4,5,7] and num = 2, return [1,5,7]
 def my_filter(L, num):
 	return [x for x in L if x % num != 0]
+
+# Problem 1.7.2 my_lists(L)
+# Input: list L of non-negative integers
+# Output: a list of lists: for every element x in L create a list containing 1,2,...x.
+# Example: given [1,2,4] return [[1], [1,2],[1,2,3,4]].
+# Example: given [0] return [[]]
+def my_lists(L):
+	return [list(range(1,x+1)) for x in L]
