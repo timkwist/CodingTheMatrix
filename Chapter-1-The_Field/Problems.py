05b2c2b7e3e73ece9fb3cfbd290ec81e1c8b635c
Tim Kwist <timkwist@hotmail.com>
2016-01-01 23:20:06 -0800

    Add Problem 1.7.3

--- Chapter-1-The_Field/Problems.py
+++ Chapter-1-The_Field/Problems.py
@@ -10,10 +10,17 @@
 def my_filter(L, num):
 	return [x for x in L if x % num != 0]
 
-# Problem 1.7.2 my_lists(L)
+# Problem 1.7.2: my_lists(L)
 # Input: list L of non-negative integers
 # Output: a list of lists: for every element x in L create a list containing 1,2,...x.
 # Example: given [1,2,4] return [[1], [1,2],[1,2,3,4]].
 # Example: given [0] return [[]]
 def my_lists(L):
 	return [list(range(1,x+1)) for x in L]
+
+# Problem 1.7.3: my_function_composition(f,g)
+# Input: two functions f and g, represented as dictionaries, such that g of f exists
+# Output: dictionary that represents the function g of f
+# Example: given f = {0:'a', 1:'b'} and g = {'a':apple, 'b':'banana'}, return {0:'apple', 1:'banana'}
+def my_function_composition(f,g):
+	return {x:g[y] for x, y in f.items()}
